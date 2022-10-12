75178ac731a04b9bbf7803cf09ab92bf475921c3
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-12 16:37:53 +0200

    add first carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,61 @@ const slides = [
 ]
 
 // seleziono l'elemento della DOM dove mettere le immagini
-const imgElement = document.querySelector('.slides');
+let imgElement = document.querySelector('.slides');
 
-// seleziono i bottoni
+
+// definisco una variabile per l'immagine attiva
+let activeSlide = 0;
+
+// ciclo for per la selezione delle immagini
+for (let i = 0; i < slides.length; i++) {
+    const imgPath = slides[i];
+    const activeImg = `<img class="${i === activeSlide ? 'active' : ''}" src="${imgPath}" alt"">`;
+    imgElement.insertAdjacentHTML('beforeend', activeImg);
+}
+
+// seleziono i bottoni di controllo
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
+
+// aggiungo un EventListener al pulsante next
+nextBtn.addEventListener('click', function() {
+    console.log('next');
+
+    // cerco l'immagine attiva
+    const slidesImg = document.querySelectorAll('.slides > img');
+    const currentSlide = slidesImg[activeSlide];
+    
+    // tolgo la classe active
+    currentSlide.classList.remove('active');
+
+    // incremento
+    activeSlide++;
+
+    // seleziono la seconda immagine
+    const nextSlide = slidesImg[activeSlide];
+
+    // applico la classe active
+    nextSlide.classList.add('active');
+}) 
+
+// aggiungo un EventListener al pulsante prev
+prevBtn.addEventListener('click', function() {
+    console.log('prev');
+
+    // cerco l'immagine attiva
+    const slidesImg = document.querySelectorAll('.slides > img');
+    const currentSlide = slidesImg[activeSlide];
+    
+    // tolgo la classe active
+    currentSlide.classList.remove('active');
+
+    // incremento
+    activeSlide--;
+
+    // seleziono la seconda immagine
+    const nextSlide = slidesImg[activeSlide];
+
+    // applico la classe active
+    nextSlide.classList.add('active');
+})  
